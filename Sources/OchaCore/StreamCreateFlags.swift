dd6ba685a2d97af330678caeb4f5a2ddfad31787
bannzai <kingkong999yhirose@gmail.com>
2019-03-01 19:56:53 +0900

    :memo: Add stream create flags

---
+++ Sources/OchaCore/StreamCreateFlags.swift
@@ -0,0 +1,37 @@
+//
+//  StreamCreateFlags.swift
+//  OchaCoreTests
+//
+//  Created by Yudai.Hirose on 2019/03/01.
+//
+
+import Foundation
+
+//public var kFSEventStreamCreateFlagNone: Int { get }
+//public var kFSEventStreamCreateFlagUseCFTypes: Int { get }
+//public var kFSEventStreamCreateFlagNoDefer: Int { get }
+//public var kFSEventStreamCreateFlagWatchRoot: Int { get }
+//public var kFSEventStreamCreateFlagIgnoreSelf: Int { get }
+//public var kFSEventStreamCreateFlagFileEvents: Int { get }
+//public var kFSEventStreamCreateFlagMarkSelf: Int { get }
+//public var kFSEventStreamCreateFlagUseExtendedData: Int { get }
+
+
+//print("kFSEventStreamCreateFlagNone: \(kFSEventStreamCreateFlagNone)")
+//print("kFSEventStreamCreateFlagUseCFTypes: \(kFSEventStreamCreateFlagUseCFTypes)")
+//print("kFSEventStreamCreateFlagNoDefer: \(kFSEventStreamCreateFlagNoDefer)")
+//print("kFSEventStreamCreateFlagWatchRoot: \(kFSEventStreamCreateFlagWatchRoot)")
+//print("kFSEventStreamCreateFlagIgnoreSelf: \(kFSEventStreamCreateFlagIgnoreSelf)")
+//print("kFSEventStreamCreateFlagFileEvents: \(kFSEventStreamCreateFlagFileEvents)")
+//print("kFSEventStreamCreateFlagMarkSelf: \(kFSEventStreamCreateFlagMarkSelf)")
+//print("kFSEventStreamCreateFlagUseExtendedData: \(kFSEventStreamCreateFlagUseExtendedData)")
+
+//kFSEventStreamCreateFlagNone: 0
+//kFSEventStreamCreateFlagUseCFTypes: 1
+//kFSEventStreamCreateFlagNoDefer: 2
+//kFSEventStreamCreateFlagWatchRoot: 4
+//kFSEventStreamCreateFlagIgnoreSelf: 8
+//kFSEventStreamCreateFlagFileEvents: 16
+//kFSEventStreamCreateFlagMarkSelf: 32
+//kFSEventStreamCreateFlagUseExtendedData: 64
+//
