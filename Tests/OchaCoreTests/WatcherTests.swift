3feac5fe771292c8d938cdbf6b8eb663519d3f76
bannzai <kingkong999yhirose@gmail.com>
2019-03-01 19:11:23 +0900

    :saprkles: :construction: WIP create tests

--- Tests/OchaCoreTests/WatcherTests.swift
+++ Tests/OchaCoreTests/WatcherTests.swift
@@ -12,14 +12,20 @@ class WatcherTests: XCTestCase {
     override func setUp() {
     }
 
-    func testExample() {
-        
+    let url = URL(fileURLWithPath: TestFile.file())
+    func read() throws -> String {
+        return try String(contentsOf: url)
     }
-
+    func testExample() throws {
+        let originalContent = try read()
+        try (originalContent + "\n").write(to: url, atomically: true, encoding: .utf8)
+        
+        XCTAssertNotEqual(originalContent, try read())
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
 
