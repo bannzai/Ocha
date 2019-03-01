732d9b44f977c50deacd162268ee04cdf4151bcf
bannzai <kingkong999yhirose@gmail.com>
2019-03-01 20:30:06 +0900

    :sparkles: Watch file pattern and directory pattern

--- Sources/Ocha/main.swift
+++ Sources/Ocha/main.swift
@@ -1,13 +1,27 @@
 import Foundation
 import OchaCore
 
-let file = #file
-print("Hello, world!: \(file)")
 
-let watcher = Watcher(paths: [file])
-watcher.start { (events) in
-    print("Yeaaaaaaaaaaaaaaaaaaa!")
-    print(events)
+do {
+    let file = #file
+    print("Hello, world!: \(file)")
+    let watcher = Watcher(paths: [file])
+    watcher.start { (events) in
+        print("Yeaaaaaaaaaaaaaaaaaaa!")
+        print(events)
+    }
 }
 
-while true { }
+do {
+    let file = "/Users/hiroseyuudai/develop/oss/Ocha/Sources/ocha/"
+    print("Hello, world!: \(file)")
+    let watcher = Watcher(paths: [file])
+    watcher.start { (events) in
+        print("Yeaaaaaaaaaaaaaaaaaaa!")
+        print(events)
+    }
+}
+    
+
+RunLoop.main.run()
+
