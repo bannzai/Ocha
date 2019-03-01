f573b46bba41464186deb8c7ddab5d71eb04142e
bannzai <kingkong999yhirose@gmail.com>
2019-03-01 20:46:20 +0900

    :sparkles: Fix for presenter

--- Sources/OchaCore/Watcher.swift
+++ Sources/OchaCore/Watcher.swift
@@ -7,40 +7,52 @@
 
 import Foundation
 
+public typealias CallBack = (URL) -> Void
 public class Presenter: NSObject, NSFilePresenter {
     public var presentedItemURL: URL?
-    public let presentedItemOperationQueue: OperationQueue = OperationQueue.main
+    public let presentedItemOperationQueue: OperationQueue = .main
     
-    public func presentedItemDidChange() {
+    internal var callback: CallBack?
+
+    internal init(presentedItemURL: URL) {
+        self.presentedItemURL = presentedItemURL
+    }
+    
+//    public func presentedItemDidChange() {
+//        print(#function)
+//    }
+    
+    public func presentedSubitemDidChange(at url: URL) {
         print(#function)
+        callback?(url)
+    }
+    
+    public func presentedItemDidChangeUbiquityAttributes(_ attributes: Set<URLResourceKey>) {
+        print(#function)
+        print(attributes)
+    }
+    
+    public func presentedItemDidGain(_ version: NSFileVersion) {
+        print(#function)
+        print(version)
     }
 }
 
 public class Watcher {
-    public typealias CallBack = ([FileEvent]) -> Void
-    
     private let paths: [String]
-
+    private let presenters: [Presenter]
+    
     public init(paths: [Pathable]) {
         self.paths = paths.map { $0.pathForWatching().absoluteString }
-        self.presenter.presentedItemURL = paths[0].pathForWatching()
+        self.presenters = paths
+            .map { $0.pathForWatching() }
+            .map(Presenter.init(presentedItemURL:))
     }
     
-    private var callback: CallBack?
-    private var presenter: Presenter = Presenter()
     public func start(_ callback: @escaping CallBack) {
-        self.callback = callback
-        NSFileCoordinator.addFilePresenter(presenter)
-    }
-    
-    public func release() {
-        callback = nil
-    }
-    
-    public func stop() {
-        
-    }
-    
-    public func resume() {
+        presenters.forEach {
+            $0.callback = callback
+            NSFileCoordinator.addFilePresenter($0)
+        }
     }
 }
