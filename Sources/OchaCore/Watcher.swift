cc85fd2399f4ddd41eb11bb107c0955986133e85
bannzai <kingkong999yhirose@gmail.com>
2019-03-01 18:46:47 +0900

    :sparkles: Add file of FileEvent

--- Sources/OchaCore/Watcher.swift
+++ Sources/OchaCore/Watcher.swift
@@ -47,20 +47,3 @@ public class Watcher {
         FSEventStreamStart(stream)
     }
 }
-
-
-public final class FileEvent : NSObject {
-    public var id: UInt64
-    public var flag: UInt32
-    public var path: Pathable
-
-    public init(
-        id: UInt64,
-        flag: UInt32,
-        path: Pathable
-        ) {
-        self.id = id
-        self.flag = flag
-        self.path = path
-    }
-}
