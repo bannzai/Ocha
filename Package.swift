9f028a9ec16724835c028362a953254a2df60fb0
bannzai <kingkong999yhirose@gmail.com>
2019-03-01 17:04:17 +0900

    :sparkles: Fix package swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Ocha",
+    products: [
+        .executable(name: "ocha", targets: ["ocha"]),
+        .library(name: "OchaCore", targets: ["OchaCore"]),
+        ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -13,10 +17,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Ocha",
+            name: "ocha",
+            dependencies: ["OchaCore"]),
+        .target(
+            name: "OchaCore",
             dependencies: []),
         .testTarget(
-            name: "OchaTests",
-            dependencies: ["Ocha"]),
+            name: "OchaCoreTests",
+            dependencies: ["OchaCore"]),
     ]
 )
