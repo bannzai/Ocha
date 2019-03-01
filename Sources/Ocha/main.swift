54c293f67a0b975bb4e3a763fa55b26d7abb5414
bannzai <kingkong999yhirose@gmail.com>
2019-03-01 19:56:33 +0900

    :sparkles: Try using watcher

--- Sources/Ocha/main.swift
+++ Sources/Ocha/main.swift
@@ -1,3 +1,12 @@
 import Foundation
+import OchaCore
 
 print("Hello, world!")
+
+let watcher = Watcher(paths: [#file])
+watcher.start { (events) in
+    print("Yeaaaaaaaaaaaaaaaaaaa!")
+    print(events)
+}
+
+while true { }
