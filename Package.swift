0a9cba7c2a5dc90a5b5778a99617b4a3142a451c
bannzai <kingkong999yhirose@gmail.com>
2019-03-02 22:40:35 +0900

    :recycle: Restructure package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Ocha",
     products: [
-        .executable(name: "ocha", targets: ["ocha"]),
-        .library(name: "OchaCore", targets: ["OchaCore"]),
+        .library(name: "Ocha", targets: ["Ocha"]),
+        .executable(name: "OchaExample", targets: ["OchaExample"]),
         ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,14 +19,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ocha",
-            dependencies: ["OchaCore"]),
+            name: "Ocha",
+            dependencies: []),
         .target(
-            name: "OchaCore",
-            dependencies: ["SwiftShell", "PathKit"]),
+            name: "OchaExample",
+            dependencies: ["Ocha", "SwiftShell", "PathKit"]),
         .testTarget(
-            name: "OchaCoreTests",
-            dependencies: ["OchaCore"]),
+            name: "OchaTests",
+            dependencies: ["Ocha"]),
     ]
 )
 
