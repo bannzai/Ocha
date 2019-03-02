cc9bc64aec13d9a9fc7fc2aa2b884e8c1ce8e061
bannzai <kingkong999yhirose@gmail.com>
2019-03-02 15:36:14 +0900

    :recycle: Remove unused container

--- Sources/Ocha/main.swift
+++ Sources/Ocha/main.swift
@@ -24,43 +24,6 @@ watcher.start { (events) in
     print(events)
 }
 
-let (writableStream, readableStream) = SwiftShell.streams()
-class Container {
-    lazy var process: Process = {
-        let process = Process()
-        process.launchPath = "/usr/bin/env"
-        process.arguments = ["tail", "-f", "/Users/hiroseyuudai/develop/oss/Ocha/Sources/ocha/main.swift"]
-        process.standardInput = writableStream.filehandle
-        process.standardOutput = readableStream.filehandle
-        process.terminationHandler = { process in
-            guard
-                let pipe = process.standardOutput as? Pipe,
-                let handle = Optional(pipe.fileHandleForReading)
-                else {
-                    return
-            }
-
-            let data = handle.readDataToEndOfFile()
-            
-            DispatchQueue.main.async {
-                print("data: \(data)")
-                handle.closeFile()
-                pipe.fileHandleForReading.readabilityHandler = nil
-            }
-        }
-        
-        return process
-    }()
-    
-    func launch() {
-        process.launch()
-        process.waitUntilExit()
-    }
-}
-
-
-//Container().launch()
-
 RunLoop.current.run()
 print("Process End")
 
