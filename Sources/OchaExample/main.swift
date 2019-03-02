e93a1735a9c61b8830b0c4a5b47ae28c03d7d281
bannzai <kingkong999yhirose@gmail.com>
2019-03-02 22:50:56 +0900

    :recycle: Fix Ocha Example

--- Sources/OchaExample/main.swift
+++ Sources/OchaExample/main.swift
@@ -4,31 +4,18 @@ import SwiftShell
 import PathKit
 
 
-//do {
-//    let file = #file
-//    print("Hello, world!: \(file)")
-//    let watcher = Watcher(paths: [file])
-//    watcher.start { (events) in
-//        print("Yeaaaaaaaaaaaaaaaaaaa!")
-//        print(events)
-//    }
-//}
+print("You can confirm for `Ocha` is watched file changes when edit and save this file.")
 
-
-let path = Path("/Users/hiroseyuudai/develop/oss/Ocha/Sources/Ocha")
-let file = path.absolute().string
-print("exists: \(path.exists)")
-print("Hello, world!: \(file)")
-let watcher = Watcher(paths: [file])
+let path = Path(#file)
+let pathString = path.absolute().string
+let watcher = Watcher(paths: [pathString])
 watcher.start { (events) in
-    print("Yeaaaaaaaaaaaaaaaaaaa!")
     print(events)
     if events.isAddedFileInXcodeEvent() {
         print("xcode added: \(events.extractAddedFileWhenAddedFromXcode())")
     }
     events.forEach { event in
         print("------- path: \(event.path) --------")
-//        print("cat: " + main.run(bash: "cat \(event.path)").stdout)
         EventSet.allCases.forEach { set in
             print("\(set): \(event.flag.contains(set))")
         }
@@ -36,4 +23,3 @@ watcher.start { (events) in
 }
 
 RunLoop.current.run()
-print("Process End")
