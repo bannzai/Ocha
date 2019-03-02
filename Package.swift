04697ffb67ab55dd3d079ec71f4e2241df014e82
bannzai <kingkong999yhirose@gmail.com>
2019-03-02 13:16:40 +0900

    :syringe: Add swift shell

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kareman/SwiftShell.git", from: Version(4, 1, 2)),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
