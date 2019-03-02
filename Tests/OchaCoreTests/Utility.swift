2b53814f61b1361c0bcc426807427bcd59ef0db3
bannzai <kingkong999yhirose@gmail.com>
2019-03-02 16:34:58 +0900

    :recycle: Separate utility files

---
+++ Tests/OchaCoreTests/Utility.swift
@@ -0,0 +1,61 @@
+//
+//  Utility.swift
+//  OchaCoreTests
+//
+//  Created by Yudai.Hirose on 2019/03/02.
+//
+
+import Foundation
+import XCTest
+@testable import OchaCore
+
+let lineBreak = "\n"
+class Utility {
+    let url: URL
+    init(url: URL) {
+        self.url = url
+    }
+    init(urlString: String) {
+        self.url = URL(fileURLWithPath: urlString)
+    }
+
+    func read() throws -> String {
+        return try String(contentsOf: url)
+    }
+    
+    func write(_ content: String) throws {
+        try content.write(to: url, atomically: true, encoding: .utf8)
+    }
+}
+
+class UtilityTests: XCTestCase {
+    private let url = URL(fileURLWithPath: TestFile.file())
+    private var utility: Utility!
+    
+    override func setUp() {
+        super.setUp()
+        
+        utility = Utility(url: url)
+    }
+    
+    private func read() throws -> String {
+        return try utility.read()
+    }
+    
+    private func write(_ content: String) throws {
+        try utility.write(content)
+    }
+
+    func testUtilityFunctions() throws {
+        let originalContent = try read()
+        try write(originalContent + lineBreak)
+        XCTAssertNotEqual(originalContent, try read())
+        XCTAssertEqual(originalContent + lineBreak, try read())
+        
+        var addedLineBreakContent = try read()
+        addedLineBreakContent.removeLast(lineBreak.count)
+        try write(addedLineBreakContent)
+        XCTAssertNotEqual(addedLineBreakContent, originalContent + lineBreak)
+        XCTAssertEqual(originalContent, try read())
+    }
+}
--- Tests/OchaCoreTests/WatcherTests.swift
+++ Tests/OchaCoreTests/WatcherTests.swift
@@ -8,13 +8,11 @@
 import XCTest
 @testable import OchaCore
 
-private let lineBreak = "\n"
-
 class WatcherTests: XCTestCase {
-
     private let url = TestFile.file()
-    func testConfirmUtilityFunction() throws {
-        let utility = Utility()
+    
+    func testWatcher() throws {
+        let utility = Utility(urlString: url)
         let watcher = Watcher(paths: [url])
 
         let ext = self.expectation(description: #function)
@@ -29,36 +27,5 @@ class WatcherTests: XCTestCase {
         
         watcher.stop()
         watcher.release()
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-        }
-    }
-
-}
-
-class Utility: XCTestCase {
-    private let url = URL(fileURLWithPath: TestFile.file())
-    func read() throws -> String {
-        return try String(contentsOf: url)
-    }
-    
-    func write(_ content: String) throws {
-        try content.write(to: url, atomically: true, encoding: .utf8)
-    }
-    
-    func testUtilityFunctions() throws {
-        let originalContent = try read()
-        try write(originalContent + lineBreak)
-        XCTAssertNotEqual(originalContent, try read())
-        XCTAssertEqual(originalContent + lineBreak, try read())
-
-        var addedLineBreakContent = try read()
-        addedLineBreakContent.removeLast(lineBreak.count)
-        try write(addedLineBreakContent)
-        XCTAssertNotEqual(addedLineBreakContent, originalContent + lineBreak)
-        XCTAssertEqual(originalContent, try read())
     }
 }
