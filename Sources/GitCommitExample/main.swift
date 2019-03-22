49eb6f6d9d89b48b309ab95a3a6ae12e743a9c96
bannzai <kingkong999yhirose@gmail.com>
2019-03-22 11:52:37 +0900

    :shell: mv Sources/SwiftLintExample -> Sources/GitcommitExmaple

---
+++ Sources/GitCommitExample/main.swift
@@ -0,0 +1,51 @@
+import Foundation
+import Ocha
+import SwiftShell
+import PathKit
+import SwiftLintFramework
+
+print("You can confirm for `Ocha` is watched file changes when edit and save this file.")
+
+let path = Path(#file)
+let pathString = path.absolute().string
+
+func hoge(fuga: String, piyo: Int) {
+    
+}
+
+func ragnarok() {
+    do {
+        let ragnarok = try 
+        let content = try String(contentsOf: path.url)
+        let formatted = try ragnarok.formatted()
+        if content == formatted {
+            return
+        }
+        // FIXME:  How to share file resouce practice when use xcode editor.
+        // Not immediately adapt for file changes after edited file.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            try! formatted.write(to: URL(fileURLWithPath: pathString), atomically: true, encoding: .utf8)
+        }
+    } catch {
+        print("[ERROR]: \(error.localizedDescription)")
+        exit(1)
+    }
+}
+
+let watcher = Watcher(paths: [pathString])
+watcher.start { (events) in
+    print(events)
+    if events.isAddedFileInXcodeEvent() {
+        print("xcode added: \(events.extractAddedFileWhenAddedFromXcode())")
+    }
+    events.forEach { event in
+        print("------- path: \(event.path) --------")
+        EventSet.allCases.forEach { set in
+            print("\(set): \(event.flag.contains(set))")
+        }
+        ragnarok()
+    }
+}
+
+
+RunLoop.current.run()
