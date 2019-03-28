e5136c1a868ed260da54ea94b98b8d5dee11893b
bannzai <kingkong999yhirose@gmail.com>
2019-03-28 15:48:13 +0900

    :sparkles: Fix git commit example

--- Sources/GitCommitExample/main.swift
+++ Sources/GitCommitExample/main.swift
@@ -6,44 +6,22 @@ import SwiftLintFramework
 
 print("You can confirm for `Ocha` is watched file changes when edit and save this file.")
 
-let path = Path(#file)
+let path: Path = .init(
+    #file.components(separatedBy: "/")
+        .dropLast() // main.swift
+        .dropLast() // GitCommitExample
+        .dropLast() // Sources
+        .joined(separator: "/")
+)
 let pathString = path.absolute().string
 
-func hoge(fuga: String, piyo: Int) {
-    
-}
-
-func ragnarok() {
-    do {
-        let ragnarok = try 
-        let content = try String(contentsOf: path.url)
-        let formatted = try ragnarok.formatted()
-        if content == formatted {
-            return
-        }
-        // FIXME:  How to share file resouce practice when use xcode editor.
-        // Not immediately adapt for file changes after edited file.
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            try! formatted.write(to: URL(fileURLWithPath: pathString), atomically: true, encoding: .utf8)
-        }
-    } catch {
-        print("[ERROR]: \(error.localizedDescription)")
-        exit(1)
-    }
-}
-
 let watcher = Watcher(paths: [pathString])
+main.currentdirectory = pathString
 watcher.start { (events) in
     print(events)
-    if events.isAddedFileInXcodeEvent() {
-        print("xcode added: \(events.extractAddedFileWhenAddedFromXcode())")
-    }
-    events.forEach { event in
-        print("------- path: \(event.path) --------")
-        EventSet.allCases.forEach { set in
-            print("\(set): \(event.flag.contains(set))")
-        }
-        ragnarok()
+    Set(events.map { $0.path }).forEach { path in
+        main.run(bash: "git add \(path)")
+        main.run(bash: "git commit -m \"Delete file \(path)\"")
     }
 }
 
