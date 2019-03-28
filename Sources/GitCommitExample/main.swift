8271ac864c95af824de743ca80fccdc43b0ee083
bannzai <kingkong999yhirose@gmail.com>
2019-03-28 16:11:59 +0900

    :recycle: Rename variables

--- Sources/GitCommitExample/main.swift
+++ Sources/GitCommitExample/main.swift
@@ -22,10 +22,10 @@ watcher.start { (events) in
     events.forEach({ (event) in
         print("event: \(event)")
     })
-    let paths = events
+    let removedEventPaths = events
         .filter { $0.flag.contains(.removedFile) || $0.flag.contains(.removedDirectory) }
         .map { $0.path }
-    Set(paths).forEach { path in
+    Set(removedEventPaths).forEach { path in
         main.run(bash: "git add \(path)")
         main.run(bash: "git commit -m \"Delete file \(path)\"")
     }
