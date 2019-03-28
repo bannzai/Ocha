5cf7984d6edb42ad9564c66db142e1b9eef0f343
bannzai <kingkong999yhirose@gmail.com>
2019-03-28 16:36:56 +0900

    :recycle: Remove unnecessary import

--- Sources/GitCommitExample/main.swift
+++ Sources/GitCommitExample/main.swift
@@ -2,7 +2,6 @@ import Foundation
 import Ocha
 import SwiftShell
 import PathKit
-import SwiftLintFramework
 
 print("You can confirm `Ocha.GitCommitExample` when removed file. Try rm -rf Package.swift and conform git history(e.g git show. You can got new commit for it message of 'Delete file [Package.swift] path'. ")
 
